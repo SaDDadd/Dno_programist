67bf92a280d57f3915dc64945d076e8664f2ba72
SaDDadd <danya.pogozhiy@mail.ru>
2024-01-15 14:24:03 +0300

    hu

---
+++ Задачи по Python/EGE/15 delenie.py
@@ -0,0 +1,9 @@
+2
+# for a in range(1000,0,-1):
+#     v=0
+#     for x in range(1,10001):
+#         if (x%10==0) and (x%26==0) and (not(x>=300)or (a<=x)):
+#             v+=1
+#     if v==10000:
+#         print(a)
+#         break
---
+++ Задачи по Python/EGE/2 zadanie.py
@@ -0,0 +1,26 @@
+# print('x','y','z')
+# for x in range(0,2):
+#     for y in range(0,2):
+#         for z in range(0,2):
+#             if not(not(x or y) or (z==x)):
+#                 print(x,y,z)
+# print('x','y','z')
+# for x in range(0,2):
+#     for y in range(0,2):
+#         for z in range(0,2):
+#             if not((x==z) or (not (x) or (y and z))):
+#                 print(x,y,z)
+# print('x','y','z','w')
+# for x in range(0,2):
+#     for y in range(0,2):
+#         for z in range(0,2):
+#             for w in range(0,2):
+#                 if ((not(x) or y)==(not(y) or z) and (y or w)):
+#                     print(x,y,z,w)
+# print('x y z w')
+# for x in range(0,2):
+#     for y in range(0,2):
+#         for z in range(0,2):
+#             for w in range(0,2):
+#                 if ((x and y) or (y and z)==((not(x) or w) and (not(w) or z))):
+#                     print(x,y,z,w)
